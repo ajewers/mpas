1d66578a3708e35847647e3384f77230a8fb81b7
Ali <amj523@york.ac.uk>
2017-02-21 15:43:56 +0000

    Created audio handler class. Added energy calculation and visualisation.

--- qttest.py
+++ qttest.py
@@ -1,16 +1,50 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import QTimer
+from PyQt4.QtGui import QPen
+from audioHandler import AudioHandler
 
+class MPASApp(QtGui.QMainWindow):
+    def __init__(self):
+        super(MPASApp, self).__init__()
+        
+        self.y = 0
+        
+        self.audioHandler = AudioHandler()
+        
+        self.initUI()
+        
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.tick)
+        self.timer.start(16)
+        
+    def initUI(self):
+        self.setGeometry(300, 300, 1600, 900)
+        self.setWindowTitle('MPAS Visual Metronome')
+        self.show()
+        
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        pen = QPen(QtCore.Qt.red, 3)
+        qp.setPen(pen)
+        
+        for i, e in enumerate(self.audioHandler.energyQueue):
+            qp.drawLine(100 + i * 20, 800, 100 + i * 20, 800 - 200 * e)
+            
+        qp.end()
+        
+    def tick(self):
+        self.y = self.y + 1
+        self.repaint()
+        
+    def closeEvent(self, event):
+        self.audioHandler.close()
+        
 def main():
-  app = QtGui.QApplication(sys.argv)
-  
-  w = QtGui.QWidget()
-  w.resize(250, 150)
-  w.move(300, 300)
-  w.setWindowTitle('Simple')
-  w.show()
+    app = QtGui.QApplication(sys.argv)
+    w = MPASApp()
+    sys.exit(app.exec_())        
 
-  sys.exit(app.exec_())
-  
 if __name__ == '__main__':
-  main()
+    main()
